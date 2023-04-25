05cf8cbc559b22089019b8e571e48c8cff82046f
davidcoats <david@getsafetycone.com>
2023-04-25 15:17:20 -0700

    Initial commit.

---
+++ source/R5T.T0190.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.T0190.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/R5T.T0190/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0190
+{
+	/// <summary>
+	/// Local, cloud, and network directory path strong-types.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
